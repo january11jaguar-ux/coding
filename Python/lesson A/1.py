035903651ca4b2a851df058698c6e58369259421
january11jaguar <January11jaguar@gmail.com>
2026-03-29 16:56:10 +0300

    WElcome back python

---
+++ Python/lesson A/1.py
@@ -0,0 +1,11 @@
+fname = input("Enter your name")
+print("HELLO!! AND WELCOME",fname)
+DOB = int(input("Enter the year you were born"))
+current_year=2026
+Age= current_year-DOB
+print(fname," is",Age,"Years old")
+
+print("What is 8+2... it is=", 8+2)
+
+for I in range (5):
+    print("m"*I)
