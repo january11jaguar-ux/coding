b5e034ca7de34d1efb0a478ddff17f9c53e24c1a
january11jaguar <January11jaguar@gmail.com>
2026-04-04 16:57:06 +0300

    commit to the bit

--- Python/lesson A/1.py
+++ Python/lesson A/1.py
@@ -9,3 +9,14 @@ print("What is 8+2... it is=", 8+2)
 
 for I in range (5):
     print("m"*I)
+
+number = int(input("Enter a number from 1-20: "))
+odd = (1, 3, 5, 7, 9, 11, 13, 15, 17, 19)
+even = (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+
+if number in odd:
+    print("This is an odd number")
+elif number in even:
+    print("This number is even")
+else:
+    print("Number is outside the 1-20 range")
