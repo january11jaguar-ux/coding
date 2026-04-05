6bda3352794561a6b7c99987b7503eb9b8c257ed
january11jaguar <January11jaguar@gmail.com>
2026-04-05 17:00:11 +0300

    commitment is comiiting

---
+++ Python/lesson C/Data types.py
@@ -0,0 +1,24 @@
+name="Penguin"
+age=15
+is_student=True
+weight=38.5
+
+print("Name :", name)
+print("Data type of Name is: ", type(name))
+
+print("Age :",age)
+print("Data type of Age is: ", type(age))
+
+print("Is student:", is_student)
+print("Data type of Is student is: ", type(is_student))
+
+print("Weight :", weight)
+print("Data type of Weight is: ", type(weight))
+
+#Type casting to convert data types of variables
+
+print("\n After Typecasting...")
+age=str(age)
+print("Data type of Age is :", type(age))
+weight= int(weight)
+print("Data type of Weight is :", type(weight))
---
+++ Python/lesson C/string operation.py
@@ -0,0 +1,11 @@
+# input a word
+text= str(input("Enter a string: "))
+
+#reverse string
+#using step value as -1 to literate
+
+revText= text[::-1]
+text= revText
+
+print("Reverse of given string is:")
+print(text)
