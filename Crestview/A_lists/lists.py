54b9007cc205aaa0d17c0ed4b69facec5cf38c06
january11jaguar <January11jaguar@gmail.com>
2026-05-14 10:59:16 +0300

    nyehehehwhhhe

---
+++ Crestview/A_lists/lists.py
@@ -0,0 +1,46 @@
+grade_8 = []
+
+grade_8.append("Daisy")
+print(grade_8)
+
+grade_8.append("Baraka")
+print(grade_8)
+
+grade_8.append("Isra")
+print(grade_8)
+
+grade_8.append("Ibrahim")
+print(grade_8)
+
+grade_8.append("Musab")
+print(grade_8)
+
+grade_8.append("Amina")
+print(grade_8)
+
+new_member = input("Enter the name of the new member : ")
+grade_8.append(new_member)
+print(grade_8)
+
+print(grade_8[2])
+print(grade_8[5])
+print(grade_8[3])
+
+grade_8.append("James")
+print(grade_8)
+
+grade_8[7]="James bond"
+print(grade_8)
+
+del grade_8[2]
+print(grade_8)
+
+vegetables = []
+
+for i in range(7):
+    user_entry= input ("Enter a vegetable name : ")
+    vegetables.append(user_entry)
+
+print(vegetables)
+
+
