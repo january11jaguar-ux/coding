400f39acf54592ff0535e8bbf71eac11af28cb65
january11jaguar <January11jaguar@gmail.com>
2026-05-22 08:46:04 +0300

    changed a bit

--- Crestview/A_lists/list2.py
+++ Crestview/A_lists/list2.py
@@ -5,9 +5,7 @@ print("Index numbers allowed go from 0 to",final_value)
 user_choice = input("nter a number to display one color in the list :")
 user_choice=int(user_choice)
 
-if user_choice<len(colorlist)-1 and user_choice>=0:
+if user_choice<len(colorlist) and user_choice>=0:
     print(colorlist[user_choice])
 else:
     print("Your entry is invalid. Try a number between 0-3 dont be a bozo you only gotta choose four numbers it isnt that hard")
-
-print("testing my typing skills")
