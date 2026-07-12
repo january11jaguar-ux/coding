636cc1fb725d7b52453e62298f9e1c78ffb39d9b
january11jaguar <January11jaguar@gmail.com>
2026-07-12 20:34:46 +0300

    alot of work ehehhehehh

---
+++ Python/lesson T/vehicle.py
@@ -0,0 +1,11 @@
+class vehicle :
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed =max_speed
+        self.mileage = mileage
+
+class Bus (vehicle):
+    pass
+
+school_bus = Bus("School Volvo", 180, 12)
+print ("vehicle name:", school_bus.name,"speed:", school_bus.max_speed, "mileage:", school_bus.mileage)
